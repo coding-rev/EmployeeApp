29e9a0913da51e134533205a095480080f144020
Emmanuel Owusu <emmowu10@gmail.com>
2021-10-03 06:27:06 -0700

    upgrade
    -

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -28,16 +28,9 @@ class ModelsTest(TestCase):
 		self.assertEqual(supervisor.supervisor, employeeKey)
 
 	def test_upload_logs(self):
-		employee = Employee.objects.create(first_name="Emmanuel", middle_name="Owusu",
-								date_of_graduation=datetime.datetime.now(pytz.utc),
-								date_of_employment=datetime.datetime.now(pytz.utc),
-								position="Software Engineer", salary="5000")
-		employee.save()
-		log 	= UploadLogs.objects.create(employee=employee,
-								timestamp_of_upload=datetime.datetime.now(pytz.utc),
-								number_of_employee_records_uploaded=5, status="success")
+		log 	= UploadLogs.objects.create(timestamp_of_upload=datetime.datetime.now(pytz.utc),
+								number_of_employee_records_uploaded=5, status="success", number_of_duplicate_entries=0)
 		log.save()
-		self.assertEqual(log.employee, employee)
 		self.assertEqual(log.status, "success")
 
 
